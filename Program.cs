510fa1ef0b07f15b603c679f55aa3fc6892ca779
RobCam <roberto.camacho4@gmail.com>
2024-10-15 13:22:10 +0200

    Removed space

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Finland!");
+﻿class Shoppinglist
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== SHOPPINGLISTA ===");
+        Console.WriteLine("1. - Visa hela listan");
+        Console.WriteLine("2. - Lägg till en artikel");
+        Console.WriteLine("3. - Ta bort en artikel");
+        Console.WriteLine("4. - Sök efter artikel");
+        Console.WriteLine("5. - Avsluta");
+    }
+}
