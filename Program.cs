b15e2d7332aac7b98ad3eb7e34090782e27d7243
RobCam <roberto.camacho4@gmail.com>
2024-10-15 14:13:47 +0200

    Test 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿class Shoppinglist
 {
     static void Main(string[] args)
-    {
+    {   // menu
         Console.WriteLine("=== SHOPPINGLISTA ===");
         Console.WriteLine("1. - Visa hela listan");
         Console.WriteLine("2. - Lägg till en artikel");
         Console.WriteLine("3. - Ta bort en artikel");
         Console.WriteLine("4. - Sök efter artikel");
         Console.WriteLine("5. - Avsluta");
+
+        // Se alla produkter i shoppinglistan
+        Console.WriteLine("Se hela Shoppinglistan:");
+        List<string> shoppingList = new List<string>();
+
+        shoppingList.Add("Banan");
+        shoppingList.Add("Motorsåg");
+        shoppingList.Add("Trollstav");
+
+        Console.WriteLine("Visa alla Produkter");
+        foreach (string product in shoppingList)
+        {
+            Console.WriteLine(product);
+        }
+
     }
 }
